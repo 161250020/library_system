aa667d8b4ab8fce159cae0cdaa3a61f90cb1723a
161250020 <799544882@qq.com>
2019-01-22 20:46:22 +0800

    service factory

--- Library_System/src/factory/ServiceFactory.java
+++ Library_System/src/factory/ServiceFactory.java
@@ -1,5 +1,34 @@
 package factory;
 
-public class ServiceFactory {
+import service.*;
+import service.impl.*;
 
+public class ServiceFactory {
+    public static AdminManageService getAdminManageService(){
+        return AdminManageServiceImpl.getInstance();
+    }
+
+    public static BookManageService getBookManageService(){
+        return BookManageServiceImpl.getInstance();
+    }
+
+    public static EditUserInfoOrderManageService getEditUserInfoOrderManageService(){
+        return EditUserInfoOrderManageServiceImpl.getInstance();
+    }
+
+    public static ElectronicBooksManageService getElectronicBooksManageService(){
+        return ElectronicBooksManageServiceImpl.getInstance();
+    }
+
+    public static OnlineReadModuleManageService getOnlineReadModuleManageService(){
+        return OnlineReadModuleManageServiceImpl.getInstance();
+    }
+
+    public static UserManageService getUserManageService(){
+        return UserManageServiceImpl.getInstance();
+    }
+
+    public static UserOrderManageService getUserOrderManageService(){
+        return UserOrderManageServiceImpl.getInstance();
+    }
 }
