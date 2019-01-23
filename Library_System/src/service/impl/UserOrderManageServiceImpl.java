744af349d437abfca0091aba0b8e033d22cc13e4
161250020 <799544882@qq.com>
2019-01-23 12:50:02 +0800

    finish all services

--- Library_System/src/service/impl/UserOrderManageServiceImpl.java
+++ Library_System/src/service/impl/UserOrderManageServiceImpl.java
@@ -5,6 +5,9 @@ import model.User;
 import model.UserOrder;
 import service.UserOrderManageService;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,20 +54,22 @@ public class UserOrderManageServiceImpl implements UserOrderManageService {
 
     @Override
     public void payFineAndChangeTableUserorder(String id, int fineDay) {
-        //
-
-
-
-
-
-
-
-
-
-
-
-
-
+        UserOrder uo=DaoFactory.getInstance().getUserOrderDAO().getUserOrder(id);
+        //当前时间
+        Date nowTime = new Date(System.currentTimeMillis());
+        SimpleDateFormat bartDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateStringToParse = bartDateFormat.format(nowTime);
+        java.util.Date date = null;
+        try {
+            date = bartDateFormat.parse(dateStringToParse);
+        } catch (ParseException e1) {
+            e1.printStackTrace();
+        }
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+        uo.setReturnTime(sqlDate);
+        uo.setFineDay(fineDay);
+        uo.setAlreadyPay(1);
+        DaoFactory.getInstance().getUserOrderDAO().updateUserOrder(uo);
     }
 
 //    @Override
